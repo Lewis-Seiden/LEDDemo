11a541197530faed1d276fd5e6ed5e9ac5ab946a
Highlander Robotics <frc8033@piedmontmakers.org>
2022-08-21 14:26:50 -0700

    refactored to command based

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,9 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -15,11 +15,9 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
-  //Creates an led object, and a buffer that we store what colors we want in.
-  AddressableLED led;
-  AddressableLEDBuffer buffer;
-  
-  int counter = 0;
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -27,53 +25,79 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-  
-    //sets up the leds
-    led = new AddressableLED(8); // 8 is the port the leds are on
-    // 70 leds / 2 leds per index
-    buffer = new AddressableLEDBuffer(70);
-    led.setLength(buffer.getLength());
-    led.start();
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
    *
    * <p>This runs after the mode specific periodic functions, but before LiveWindow and
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
 
-  /** This function is called once when teleop is enabled. */
+  /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void teleopInit() {}
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {
-    flashingLights(150, 255, 255);
-  }
+  public void teleopPeriodic() {}
 
-  /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {
-    setSolidLEDColor(0, 0, 0);
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
   }
 
-  private void setSolidLEDColor(int h, int s, int v){
-    for (int i = 0; i < buffer.getLength(); i ++){
-      buffer.setHSV(i, h, s, v);
-    }
-  }
-  
-  private void flashingLights(int h, int s, int v){
-    counter++;
-    if (counter % 10 > 5) {
-      setSolidLEDColor(h, s, v);
-    } else {
-      setSolidLEDColor(0, 0, 0);
-    }
-  }
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
 }
